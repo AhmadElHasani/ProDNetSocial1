3b76a91c577701c925d84a8675264f603d0cdd91
ahmad el hasani <ahmad.el.asani@gmail.com>
2025-06-26 12:12:03 +0200

    Crud operations added

--- Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74f30da4628585b08658c4d2ed7ed5ee81f67f2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce30e1cb1cebeb10a0efbf66fc278316e08a18e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
